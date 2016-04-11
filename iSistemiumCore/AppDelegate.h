700b5ac1ab0234ddfc8dbc151bcb111bf5eeda03
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2016-04-11 15:47:33 +0300

    Initial Commit

---
+++ iSistemiumCore/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iSistemiumCore
+//
+//  Created by Maxim Grigoriev on 11/04/16.
+//  Copyright © 2016 Sistemium UAB. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
