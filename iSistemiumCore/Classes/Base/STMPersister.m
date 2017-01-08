6cf4e20aa2d8e3fc1c97ec6edfdab80a702c163c
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2017-01-08 11:31:11 +0300

    add observers to persister

--- iSistemiumCore/Classes/Base/STMPersister.m
+++ iSistemiumCore/Classes/Base/STMPersister.m
@@ -9,16 +9,19 @@
 #import <Foundation/Foundation.h>
 #import "STMPersister.h"
 
+#import "STMConstants.h"
+
+
 @interface STMPersister()
 
 @property (nonatomic, strong) id <STMSession> session;
 @property (nonatomic, strong) STMDocument *document;
 
+
 @end
 
-@implementation STMPersister
 
-@synthesize document;
+@implementation STMPersister
 
 + (instancetype)initWithDocument:(STMDocument *)stmdocument forSession:(id<STMSession>)session {
     
@@ -28,6 +31,58 @@
     persister.document = stmdocument;
     
     return persister;
+    
+}
+
+- (instancetype)init {
+    
+    self = [super init];
+    if (self) {
+        [self addObservers];
+    }
+    return self;
+    
+}
+
+#pragma mark - observers
+
+- (void)addObservers {
+    
+    NSNotificationCenter *nc = [NSNotificationCenter defaultCenter];
+
+    [nc addObserver:self
+           selector:@selector(sessionStatusChanged:)
+               name:NOTIFICATION_SESSION_STATUS_CHANGED
+             object:self.session];
+
+    [nc addObserver:self
+           selector:@selector(documentReady:)
+               name:NOTIFICATION_DOCUMENT_READY
+             object:nil];
+    
+    [nc addObserver:self
+           selector:@selector(documentNotReady:)
+               name:NOTIFICATION_DOCUMENT_NOT_READY
+             object:nil];
+
+}
+
+- (void)removeObservers {
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    
+}
+
+- (void)sessionStatusChanged:(NSNotification *)notification {
+    
+}
+
+- (void)documentReady:(NSNotification *)notification {
+    
+}
+
+- (void)documentNotReady:(NSNotification *)notification {
+    
 }
 
 
