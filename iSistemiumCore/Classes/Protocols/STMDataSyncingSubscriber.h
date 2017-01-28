864aa4c0d0336c99e89273ad958af570fe64c72b
gogbajbobo <grigoriev.maxim@gmail.com>
2017-01-28 16:03:49 +0300

    add STMDataSyncingSubscriber protocol

---
+++ iSistemiumCore/Classes/Protocols/STMDataSyncingSubscriber.h
@@ -0,0 +1,16 @@
+//
+//  STMDataSyncingSubscriber.h
+//  iSisSales
+//
+//  Created by Maxim Grigoriev on 28/01/2017.
+//  Copyright © 2017 Sistemium UAB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol STMDataSyncingSubscriber <NSObject>
+
+- (void)haveUnsyncedObjectWithEntityName:(NSString *)entityName itemData:(NSDictionary *)itemData itemVersion:(NSString *)itemVersion;
+
+
+@end
