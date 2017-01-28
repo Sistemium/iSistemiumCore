72b4ce486c532ede71509a4142bc6112bd8df894
gogbajbobo <grigoriev.maxim@gmail.com>
2017-01-28 14:46:48 +0300

    add STMPersistingWithHeadersAsync protocol

---
+++ iSistemiumCore/Classes/Protocols/Persisting/STMPersistingWithHeadersAsync.h
@@ -0,0 +1,29 @@
+//
+//  STMPersistingWithHeadersAsync.h
+//  iSisSales
+//
+//  Created by Maxim Grigoriev on 27/01/2017.
+//  Copyright © 2017 Sistemium UAB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol STMPersistingWithHeadersAsync <NSObject>
+
+- (void)findAsync:(NSString *)entityName
+       identifier:(NSString *)identifier
+          options:(NSDictionary *)options
+completionHandler:(void (^)(BOOL success, NSDictionary *result, NSDictionary *headers, NSError *error))completionHandler;
+
+- (void)findAllAsync:(NSString *)entityName
+           predicate:(NSPredicate *)predicate
+             options:(NSDictionary *)options
+   completionHandler:(void (^)(BOOL success, NSArray *result, NSDictionary *headers, NSError *error))completionHandler;
+
+- (void)mergeAsync:(NSString *)entityName
+        attributes:(NSDictionary *)attributes
+           options:(NSDictionary *)options
+ completionHandler:(void (^)(BOOL success, NSDictionary *result, NSDictionary *headers, NSError *error))completionHandler;
+
+
+@end
