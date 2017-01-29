4ab5100e1d7b3dcee40dd170e922e1fee495af65
Alexander Levin <alevin.ru@gmail.com>
2017-01-29 15:39:49 +0200

    typedef STMPersistingWithHeadersAsync callbacks

--- iSistemiumCore/Classes/Protocols/Persisting/STMPersistingWithHeadersAsync.h
+++ iSistemiumCore/Classes/Protocols/Persisting/STMPersistingWithHeadersAsync.h
@@ -8,22 +8,25 @@
 
 #import <Foundation/Foundation.h>
 
+typedef void (^STMPersistingWithHeadersAsyncArrayResultCallback)(BOOL success, NSArray *result, NSDictionary *headers, NSError *error);
+typedef void (^STMPersistingWithHeadersAsyncDictionaryResultCallback)(BOOL success, NSDictionary *result, NSDictionary *headers, NSError *error);
+
 @protocol STMPersistingWithHeadersAsync <NSObject>
 
 - (void)findAsync:(NSString *)entityName
        identifier:(NSString *)identifier
           options:(NSDictionary *)options
-completionHandlerWithHeaders:(void (^)(BOOL success, NSDictionary *result, NSDictionary *headers, NSError *error))completionHandler;
+completionHandlerWithHeaders:(STMPersistingWithHeadersAsyncDictionaryResultCallback)completionHandler;
 
 - (void)findAllAsync:(NSString *)entityName
            predicate:(NSPredicate *)predicate
              options:(NSDictionary *)options
-   completionHandlerWithHeaders:(void (^)(BOOL success, NSArray *result, NSDictionary *headers, NSError *error))completionHandler;
+   completionHandlerWithHeaders:(STMPersistingWithHeadersAsyncArrayResultCallback)completionHandler;
 
 - (void)mergeAsync:(NSString *)entityName
         attributes:(NSDictionary *)attributes
            options:(NSDictionary *)options
- completionHandlerWithHeaders:(void (^)(BOOL success, NSDictionary *result, NSDictionary *headers, NSError *error))completionHandler;
+ completionHandlerWithHeaders:(STMPersistingWithHeadersAsyncDictionaryResultCallback)completionHandler;
 
 
 @end
