fe7a84937213a9a0600d5bbe090d1ae8137776ff
Alexander Levin <alevin.ru@gmail.com>
2017-01-30 10:49:31 +0200

    STMPersister+CoreData.h formatting

--- iSistemiumCore/Classes/Base/Persisting/STMPersister+CoreData.h
+++ iSistemiumCore/Classes/Base/Persisting/STMPersister+CoreData.h
@@ -11,17 +11,51 @@
 @interface STMPersister (CoreData)
 
 - (void)removeObjects:(NSArray*)objects;
-- (void)removeObjectForPredicate:(NSPredicate*)predicate entityName:(NSString *)name;
 
-- (NSArray *)objectsForPredicate:(NSPredicate *)predicate entityName:(NSString *)entityName;
+- (void)removeObjectForPredicate:(NSPredicate*)predicate
+                      entityName:(NSString *)name;
+
+- (NSArray *)objectsForPredicate:(NSPredicate *)predicate
+                      entityName:(NSString *)entityName;
+
 - (NSArray *)objectsForEntityName:(NSString *)entityName;
 
-- (NSArray *)objectsForEntityName:(NSString *)entityName orderBy:(NSString *)orderBy ascending:(BOOL)ascending fetchLimit:(NSUInteger)fetchLimit withFantoms:(BOOL)withFantoms inManagedObjectContext:(NSManagedObjectContext *)context error:(NSError **)error;
+- (NSArray *)objectsForEntityName:(NSString *)entityName
+                          orderBy:(NSString *)orderBy
+                        ascending:(BOOL)ascending
+                       fetchLimit:(NSUInteger)fetchLimit
+                      withFantoms:(BOOL)withFantoms
+           inManagedObjectContext:(NSManagedObjectContext *)context
+                            error:(NSError **)error;
 
-- (NSArray *)objectsForEntityName:(NSString *)entityName orderBy:(NSString *)orderBy ascending:(BOOL)ascending fetchLimit:(NSUInteger)fetchLimit fetchOffset:(NSUInteger)fetchOffset withFantoms:(BOOL)withFantoms inManagedObjectContext:(NSManagedObjectContext *)context error:(NSError **)error;
+- (NSArray *)objectsForEntityName:(NSString *)entityName
+                          orderBy:(NSString *)orderBy
+                        ascending:(BOOL)ascending
+                       fetchLimit:(NSUInteger)fetchLimit
+                      fetchOffset:(NSUInteger)fetchOffset
+                      withFantoms:(BOOL)withFantoms
+           inManagedObjectContext:(NSManagedObjectContext *)context
+                            error:(NSError **)error;
 
-- (NSArray *)objectsForEntityName:(NSString *)entityName orderBy:(NSString *)orderBy ascending:(BOOL)ascending fetchLimit:(NSUInteger)fetchLimit fetchOffset:(NSUInteger)fetchOffset withFantoms:(BOOL)withFantoms predicate:(NSPredicate *)predicate inManagedObjectContext:(NSManagedObjectContext *)context error:(NSError **)error;
+- (NSArray *)objectsForEntityName:(NSString *)entityName
+                          orderBy:(NSString *)orderBy
+                        ascending:(BOOL)ascending
+                       fetchLimit:(NSUInteger)fetchLimit
+                      fetchOffset:(NSUInteger)fetchOffset
+                      withFantoms:(BOOL)withFantoms
+                        predicate:(NSPredicate *)predicate
+           inManagedObjectContext:(NSManagedObjectContext *)context
+                            error:(NSError **)error;
 
-- (NSArray *)objectsForEntityName:(NSString *)entityName orderBy:(NSString *)orderBy ascending:(BOOL)ascending fetchLimit:(NSUInteger)fetchLimit fetchOffset:(NSUInteger)fetchOffset withFantoms:(BOOL)withFantoms predicate:(NSPredicate *)predicate resultType:(NSFetchRequestResultType)resultType inManagedObjectContext:(NSManagedObjectContext *)context error:(NSError **)error;
+- (NSArray *)objectsForEntityName:(NSString *)entityName
+                          orderBy:(NSString *)orderBy
+                        ascending:(BOOL)ascending
+                       fetchLimit:(NSUInteger)fetchLimit
+                      fetchOffset:(NSUInteger)fetchOffset
+                      withFantoms:(BOOL)withFantoms
+                        predicate:(NSPredicate *)predicate
+                       resultType:(NSFetchRequestResultType)resultType
+           inManagedObjectContext:(NSManagedObjectContext *)context
+                            error:(NSError **)error;
 
 @end
