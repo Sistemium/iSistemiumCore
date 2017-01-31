ca0340aa572a6496d79cfaa8ce729aaab2c3137c
Alexander Levin <alevin.ru@gmail.com>
2017-01-31 15:21:31 +0200

    STMPersistingSpeedTests

---
+++ iSistemiumCoreTests/STMPersistingSpeedTests.m
@@ -0,0 +1,59 @@
+//
+//  STMPersistingSpeedTests.m
+//  iSisSales
+//
+//  Created by Alexander Levin on 31/01/2017.
+//  Copyright © 2017 Sistemium UAB. All rights reserved.
+//
+
+#import "STMPersistingTests.h"
+
+@interface STMPersistingSpeedTests : STMPersistingTests
+
+@end
+
+@implementation STMPersistingSpeedTests
+
+- (void)setUp {
+    [super setUp];
+}
+
+
+- (void)testBunchOfFindSpeed {
+    
+    NSString *sourceEntityName = @"STMOutlet";
+    NSString *parentEntity = @"STMPartner";
+    NSString *parentKey = @"partnerId";
+    
+    __block NSError *error;
+    
+    NSArray *sourceArray = [self.persister findAllSync:sourceEntityName
+                                             predicate:nil
+                                               options:@{STMPersistingOptionPageSize:@10000}
+                                                 error:&error];
+    XCTAssertNil(error);
+    XCTAssertNotEqual(sourceArray.count, 0, @"There should be some sourceArray data to test");
+    
+    NSLog(@"testFindSpeed will do %lu find requests to '%@'",
+          (unsigned long)sourceArray.count, parentEntity);
+    
+    NSDate *startedAt = [NSDate date];
+    
+    [self measureBlock:^{
+        for (NSDictionary *item in sourceArray) {
+            
+            if (!item[parentKey]) continue;
+            
+            [self.persister findSync:parentEntity
+                          identifier:item[parentKey]
+                             options:nil
+                               error:&error];
+            
+            XCTAssertNil(error);
+        }
+    }];
+    
+    NSLog(@"testFindSpeed took measureBlock run %f seconds", -[startedAt timeIntervalSinceNow]);
+}
+
+@end
