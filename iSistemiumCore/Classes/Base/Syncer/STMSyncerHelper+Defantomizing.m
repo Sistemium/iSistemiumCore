1eed401e4a92d680a50799990d993ba779f2718a
Alexander Levin <alevin.ru@gmail.com>
2017-02-04 23:54:42 +0200

    synchronized(self) in SyncerHelper+Defantomizing

--- iSistemiumCore/Classes/Base/Syncer/STMSyncerHelper+Defantomizing.m
+++ iSistemiumCore/Classes/Base/Syncer/STMSyncerHelper+Defantomizing.m
@@ -103,7 +103,7 @@
         
     } else {
         
-        @synchronized (self.failToResolveFantomsArray) {
+        @synchronized (self) {
             [self.failToResolveFantomsArray addObject:fantomDic];
         }
         
