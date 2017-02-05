7a2e75f092e5995ff9c3fa004061357bf8548cd5
gogbajbobo <grigoriev.maxim@gmail.com>
2017-02-05 11:46:42 +0300

    add STMSyncerHelper+Downloading category

---
+++ iSistemiumCore/Classes/Base/Syncer/STMSyncerHelper+Downloading.m
@@ -0,0 +1,13 @@
+//
+//  STMSyncerHelper+Downloading.m
+//  iSisSales
+//
+//  Created by Maxim Grigoriev on 05/02/2017.
+//  Copyright © 2017 Sistemium UAB. All rights reserved.
+//
+
+#import "STMSyncerHelper+Downloading.h"
+
+@implementation STMSyncerHelper (Downloading)
+
+@end
