eca01921c44e7d4eb1adb93b70f63cde26ceb666
Alexander Levin <alevin.ru@gmail.com>
2017-02-06 19:38:58 +0200

    PersistingAsyncTests testErrors

--- iSistemiumCoreTests/PersistingAsyncTests.m
+++ iSistemiumCoreTests/PersistingAsyncTests.m
@@ -15,5 +15,51 @@
 @implementation PersistingAsyncTests
 
 
+-(void)testErrors{
+    
+    [self.fakePersiser setOption:STMFakePersistingOptionCheckModelKey
+                           value:@(YES)];
+    
+    NSString *entityName = @"UnknownEntity";
+    
+    XCTestExpectation *findAllAsync = [self expectationWithDescription:@"findAllAsync"];
+    
+    [self.persister findAllAsync:entityName
+                       predicate:nil
+                         options:nil
+               completionHandler:^(BOOL success, NSArray *result, NSError *error) {
+                   XCTAssertNotNil(error);
+                   XCTAssertFalse(success);
+                   [findAllAsync fulfill];
+               }];
+
+    [self.persister destroyAllAsync:entityName
+                          predicate:nil
+                            options:nil
+                  completionHandler:^(BOOL success, NSUInteger result, NSError *error) {
+                      XCTAssertNotNil(error);
+                      XCTAssertFalse(success);
+                  }];
+    
+    [self.persister findAsync:entityName
+                    identifier:entityName
+                      options:nil
+            completionHandler:^(BOOL success, NSDictionary *result, NSError *error) {
+                XCTAssertNotNil(error);
+                XCTAssertFalse(success);
+            }];
+    
+    [self.persister destroyAsync:entityName
+                      identifier:entityName
+                         options:nil
+               completionHandler:^(BOOL success, NSError *error) {
+                   XCTAssertNotNil(error);
+                   XCTAssertFalse(success);
+               }];
+
+    [self waitForExpectationsWithTimeout:1 handler:nil];
+
+}
+
 
 @end
