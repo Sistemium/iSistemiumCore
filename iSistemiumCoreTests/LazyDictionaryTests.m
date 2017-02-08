d6e67e90d618de25a19e017b10a501640934b63a
Alexander Levin <alevin.ru@gmail.com>
2017-02-08 18:48:44 +0200

    LazyDictionaryTests

---
+++ iSistemiumCoreTests/LazyDictionaryTests.m
@@ -0,0 +1,69 @@
+//
+//  LazyDictionaryTests.m
+//  iSisSales
+//
+//  Created by Alexander Levin on 08/02/2017.
+//  Copyright © 2017 Sistemium UAB. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "STMLazyDictionary.h"
+
+@interface LazyDictionaryTests : XCTestCase
+
+@end
+
+@implementation LazyDictionaryTests
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)testLazyDictionaryUsage {
+    
+    Class itemsClass = NSMutableArray.class;
+
+    STMLazyDictionary *dictionary = [STMLazyDictionary lazyDictionaryWithItemsClass:itemsClass];
+    
+    // dictionary value will be auto-instantiated with ItemsClass on first access
+    
+    NSString *KEY = @"key";
+    
+    XCTAssertNotNil(dictionary[KEY]);
+    XCTAssertTrue([[dictionary[KEY] class] isSubclassOfClass:itemsClass]);
+
+    // assigning with square brackets is supported
+    
+    NSString *KEY2 = @"key2";
+    NSString *value = @"value for key 2";
+    
+    [dictionary[KEY2] addObject:value];
+    
+    XCTAssertEqualObjects([dictionary[KEY2] firstObject], value);
+    
+    // can assigning any object when assigning
+    
+    dictionary[KEY2] = value;
+    
+    XCTAssertEqualObjects(dictionary[KEY2], value);
+    
+    // assign nil to remove key
+    
+    dictionary[KEY2] = nil;
+    
+    // use hasKey: to test if there's a key
+    
+    XCTAssertFalse([dictionary hasKey:KEY2]);
+    
+    // also there are named equivalent methods for reading, writing and removing
+    
+    [dictionary setObject:value forKey:KEY2];
+    XCTAssertEqualObjects([dictionary valueForKey:KEY2], value);
+    XCTAssertEqualObjects([dictionary objectForKey:KEY2], value);
+    
+    [dictionary removeObjectForKey:KEY2];
+    XCTAssertFalse([dictionary hasKey:KEY2]);
+    
+}
+
+@end
