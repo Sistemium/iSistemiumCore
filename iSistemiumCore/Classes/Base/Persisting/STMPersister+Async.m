fd97572ec8d48c801526f5043df78766378b76e4
Alexander Levin <alevin.ru@gmail.com>
2017-02-09 22:47:03 +0200

    STMPersister+Async with template

--- iSistemiumCore/Classes/Base/Persisting/STMPersister+Async.m
+++ iSistemiumCore/Classes/Base/Persisting/STMPersister+Async.m
@@ -2,122 +2,238 @@
 //  STMPersister+Async.m
 //  iSisSales
 //
-//  Created by Alexander Levin on 25/01/2017.
+//  Generated with HandlebarsGenerator on Thu, 09 Feb 2017 20:43:36 GMT
+//  Don't edit this file directly!
+//
 //  Copyright © 2017 Sistemium UAB. All rights reserved.
 //
 
 #import "STMPersister+Async.h"
-#import "STMFmdb.h"
 
-
-#define STM_PERSISTER_ASYNC_DISPATCH_QUEUE DISPATCH_QUEUE_PRIORITY_DEFAULT
-#define STM_PERSISTER_PROMISED_DISPATCH_QUEUE DISPATCH_QUEUE_PRIORITY_DEFAULT
-
-
-#define STMPersisterAsyncWithSync(resultType,methodName,signatureAttributes) \
-dispatch_async(dispatch_get_global_queue(STM_PERSISTER_ASYNC_DISPATCH_QUEUE, 0), ^{ \
-    NSError *error; \
-    resultType result = [self methodName##Sync:entityName signatureAttributes:signatureAttributes options:options error:&error]; \
-        if (completionHandler) completionHandler(!error,result,error); \
-    });
-
-#define STMPersisterPromisedWithSyncScalar(returnType,methodName,signatureAttributes) \
-return [AnyPromise promiseWithResolverBlock:^(PMKResolver resolve){ \
-    dispatch_async(dispatch_get_global_queue(STM_PERSISTER_PROMISED_DISPATCH_QUEUE, 0), ^{ \
-        NSError *error; \
-        returnType result = [self methodName##Sync:entityName signatureAttributes:signatureAttributes options:options error:&error]; \
-        if (error) resolve(error); else resolve(@(result)); \
-    }); \
-}];
-
-#define STMPersisterPromisedWithSync(returnType,methodName,signatureAttributes) \
-return [AnyPromise promiseWithResolverBlock:^(PMKResolver resolve){ \
-    dispatch_async(dispatch_get_global_queue(STM_PERSISTER_PROMISED_DISPATCH_QUEUE, 0), ^{ \
-        NSError *error; \
-        returnType *result = [self methodName##Sync:entityName signatureAttributes:signatureAttributes options:options error:&error]; \
-        if (error) resolve(error); else resolve(result); \
-    }); \
-}];
+#define STM_PERSISTER_DISPATCH_QUEUE DISPATCH_QUEUE_PRIORITY_DEFAULT
 
 
 @implementation STMPersister (Async)
 
 - (void)findAsync:(NSString *)entityName identifier:(NSString *)identifier options:(NSDictionary *)options completionHandler:(STMPersistingAsyncDictionaryResultCallback)completionHandler {
-    STMPersisterAsyncWithSync(NSDictionary *,find,identifier)
+
+    dispatch_async(dispatch_get_global_queue(STM_PERSISTER_DISPATCH_QUEUE, 0), ^{
+
+        NSError *error;
+        NSDictionary * result = [self findSync:entityName identifier:identifier options:options error:&error];
+        if (completionHandler) completionHandler(!error, result, error);
+
+   });
+
 }
 
 - (void)findAllAsync:(NSString *)entityName predicate:(NSPredicate *)predicate options:(NSDictionary *)options completionHandler:(STMPersistingAsyncArrayResultCallback)completionHandler {
-    STMPersisterAsyncWithSync(NSArray *,findAll,predicate)
+
+    dispatch_async(dispatch_get_global_queue(STM_PERSISTER_DISPATCH_QUEUE, 0), ^{
+
+        NSError *error;
+        NSArray * result = [self findAllSync:entityName predicate:predicate options:options error:&error];
+        if (completionHandler) completionHandler(!error, result, error);
+
+   });
+
 }
 
 - (void)mergeAsync:(NSString *)entityName attributes:(NSDictionary *)attributes options:(NSDictionary *)options completionHandler:(STMPersistingAsyncDictionaryResultCallback)completionHandler {
-    STMPersisterAsyncWithSync(NSDictionary *,merge,attributes)
+
+    dispatch_async(dispatch_get_global_queue(STM_PERSISTER_DISPATCH_QUEUE, 0), ^{
+
+        NSError *error;
+        NSDictionary * result = [self mergeSync:entityName attributes:attributes options:options error:&error];
+        if (completionHandler) completionHandler(!error, result, error);
+
+   });
+
 }
 
 - (void)mergeManyAsync:(NSString *)entityName attributeArray:(NSArray *)attributeArray options:(NSDictionary *)options completionHandler:(STMPersistingAsyncArrayResultCallback)completionHandler {
-    STMPersisterAsyncWithSync(NSArray *,mergeMany,attributeArray)
+
+    dispatch_async(dispatch_get_global_queue(STM_PERSISTER_DISPATCH_QUEUE, 0), ^{
+
+        NSError *error;
+        NSArray * result = [self mergeManySync:entityName attributeArray:attributeArray options:options error:&error];
+        if (completionHandler) completionHandler(!error, result, error);
+
+   });
+
 }
 
 - (void)destroyAsync:(NSString *)entityName identifier:(NSString *)identifier options:(NSDictionary *)options completionHandler:(STMPersistingAsyncNoResultCallback)completionHandler {
-    dispatch_async(dispatch_get_global_queue(STM_PERSISTER_ASYNC_DISPATCH_QUEUE, 0), ^{
+
+    dispatch_async(dispatch_get_global_queue(STM_PERSISTER_DISPATCH_QUEUE, 0), ^{
+
         NSError *error;
-        BOOL result = [self destroySync:entityName identifier:identifier options:options error:&error];
-        if (completionHandler) completionHandler(result,error);
-    });
+        [self destroySync:entityName identifier:identifier options:options error:&error];
+        if (completionHandler) completionHandler(!error, error);
+
+   });
+
 }
 
 - (void)destroyAllAsync:(NSString *)entityName predicate:(NSPredicate *)predicate options:(NSDictionary *)options completionHandler:(STMPersistingAsyncIntegerResultCallback)completionHandler {
-    STMPersisterAsyncWithSync(NSUInteger,destroyAll,predicate)
+
+    dispatch_async(dispatch_get_global_queue(STM_PERSISTER_DISPATCH_QUEUE, 0), ^{
+
+        NSError *error;
+        NSUInteger result = [self destroyAllSync:entityName predicate:predicate options:options error:&error];
+        if (completionHandler) completionHandler(!error, result, error);
+
+   });
+
 }
 
 - (void)updateAsync:(NSString *)entityName attributes:(NSDictionary *)attributes options:(NSDictionary *)options completionHandler:(STMPersistingAsyncDictionaryResultCallback)completionHandler {
-    STMPersisterAsyncWithSync(NSDictionary *,update,attributes)
+
+    dispatch_async(dispatch_get_global_queue(STM_PERSISTER_DISPATCH_QUEUE, 0), ^{
+
+        NSError *error;
+        NSDictionary * result = [self updateSync:entityName attributes:attributes options:options error:&error];
+        if (completionHandler) completionHandler(!error, result, error);
+
+   });
+
 }
 
 
 #pragma mark - STMPersistingPromised
 
 
-- (AnyPromise *)find:(NSString *)entityName
-          identifier:(NSString *)identifier
-             options:(STMPersistingOptions)options {
-    STMPersisterPromisedWithSync(NSDictionary,find,identifier)
+- (AnyPromise *)find:(NSString *)entityName identifier:(NSString *)identifier options:(STMPersistingOptions)options {
+
+    return [AnyPromise promiseWithResolverBlock:^(PMKResolver resolve){
+        dispatch_async(dispatch_get_global_queue(STM_PERSISTER_DISPATCH_QUEUE, 0), ^{
+
+            NSError *error;
+            NSDictionary * result = [self findSync:entityName identifier:identifier options:options error:&error];
+
+            if (error) {
+                resolve(error);
+            } else {
+                resolve(result);
+            }
+
+        });
+    }];
+
 }
 
-- (AnyPromise *)findAll:(NSString *)entityName
-              predicate:(NSPredicate *)predicate
-                options:(STMPersistingOptions)options {
-    STMPersisterPromisedWithSync(NSArray,findAll,predicate)
+- (AnyPromise *)findAll:(NSString *)entityName predicate:(NSPredicate *)predicate options:(STMPersistingOptions)options {
+
+    return [AnyPromise promiseWithResolverBlock:^(PMKResolver resolve){
+        dispatch_async(dispatch_get_global_queue(STM_PERSISTER_DISPATCH_QUEUE, 0), ^{
+
+            NSError *error;
+            NSArray * result = [self findAllSync:entityName predicate:predicate options:options error:&error];
+
+            if (error) {
+                resolve(error);
+            } else {
+                resolve(result);
+            }
+
+        });
+    }];
+
 }
 
-- (AnyPromise *)merge:(NSString *)entityName
-           attributes:(NSDictionary *)attributes
-              options:(STMPersistingOptions)options {
-    STMPersisterPromisedWithSync(NSDictionary,merge,attributes)
+- (AnyPromise *)merge:(NSString *)entityName attributes:(NSDictionary *)attributes options:(STMPersistingOptions)options {
+
+    return [AnyPromise promiseWithResolverBlock:^(PMKResolver resolve){
+        dispatch_async(dispatch_get_global_queue(STM_PERSISTER_DISPATCH_QUEUE, 0), ^{
+
+            NSError *error;
+            NSDictionary * result = [self mergeSync:entityName attributes:attributes options:options error:&error];
+
+            if (error) {
+                resolve(error);
+            } else {
+                resolve(result);
+            }
+
+        });
+    }];
+
 }
 
-- (AnyPromise *)mergeMany:(NSString *)entityName
-           attributeArray:(NSArray *)attributeArray
-                  options:(STMPersistingOptions)options {
-    STMPersisterPromisedWithSync(NSArray,mergeMany,attributeArray)
+- (AnyPromise *)mergeMany:(NSString *)entityName attributeArray:(NSArray *)attributeArray options:(STMPersistingOptions)options {
+
+    return [AnyPromise promiseWithResolverBlock:^(PMKResolver resolve){
+        dispatch_async(dispatch_get_global_queue(STM_PERSISTER_DISPATCH_QUEUE, 0), ^{
+
+            NSError *error;
+            NSArray * result = [self mergeManySync:entityName attributeArray:attributeArray options:options error:&error];
+
+            if (error) {
+                resolve(error);
+            } else {
+                resolve(result);
+            }
+
+        });
+    }];
+
 }
 
-- (AnyPromise *)destroy:(NSString *)entityName
-             identifier:(NSString *)identifier
-                options:(STMPersistingOptions)options {
-    STMPersisterPromisedWithSyncScalar(BOOL,destroy,identifier)
+- (AnyPromise *)destroy:(NSString *)entityName identifier:(NSString *)identifier options:(STMPersistingOptions)options {
+
+    return [AnyPromise promiseWithResolverBlock:^(PMKResolver resolve){
+        dispatch_async(dispatch_get_global_queue(STM_PERSISTER_DISPATCH_QUEUE, 0), ^{
+
+            NSError *error;
+            BOOL result = [self destroySync:entityName identifier:identifier options:options error:&error];
+
+            if (error) {
+                resolve(error);
+            } else {
+                resolve(@(result));
+            }
+
+        });
+    }];
+
 }
 
-- (AnyPromise *)destroyAll:(NSString *)entityName
-                 predicate:(NSPredicate *)predicate
-                   options:(STMPersistingOptions)options {
-    STMPersisterPromisedWithSyncScalar(NSUInteger,destroyAll,predicate)
+- (AnyPromise *)destroyAll:(NSString *)entityName predicate:(NSPredicate *)predicate options:(STMPersistingOptions)options {
+
+    return [AnyPromise promiseWithResolverBlock:^(PMKResolver resolve){
+        dispatch_async(dispatch_get_global_queue(STM_PERSISTER_DISPATCH_QUEUE, 0), ^{
+
+            NSError *error;
+            NSUInteger result = [self destroyAllSync:entityName predicate:predicate options:options error:&error];
+
+            if (error) {
+                resolve(error);
+            } else {
+                resolve(@(result));
+            }
+
+        });
+    }];
+
 }
 
-- (AnyPromise *)update:(NSString *)entityName
-                 attributes:(NSDictionary *)attributes
-                   options:(STMPersistingOptions)options {
-    STMPersisterPromisedWithSync(NSDictionary,update,attributes)
+- (AnyPromise *)update:(NSString *)entityName attributes:(NSDictionary *)attributes options:(STMPersistingOptions)options {
+
+    return [AnyPromise promiseWithResolverBlock:^(PMKResolver resolve){
+        dispatch_async(dispatch_get_global_queue(STM_PERSISTER_DISPATCH_QUEUE, 0), ^{
+
+            NSError *error;
+            NSDictionary * result = [self updateSync:entityName attributes:attributes options:options error:&error];
+
+            if (error) {
+                resolve(error);
+            } else {
+                resolve(result);
+            }
+
+        });
+    }];
+
 }
 
 @end
---
+++ templates/iSistemiumCore/Classes/Base/Persisting/STMPersister+Async.js
@@ -0,0 +1,9 @@
+const {meta, methods} = require('../../../../STMFakePersisting');
+
+const file = {
+    className: 'STMPersister',
+    categoryName: 'Async',
+    queueConst: 'STM_PERSISTER'
+};
+
+module.exports = {methods, meta, file};
