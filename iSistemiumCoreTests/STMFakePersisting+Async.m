2c104ad1907c52887d6f2eb8582cb6ac4654ce87
Alexander Levin <alevin.ru@gmail.com>
2017-02-09 21:06:44 +0200

    STMFakePersisting with HandlebarsGenerator

--- iSistemiumCoreTests/STMFakePersisting+Async.m
+++ iSistemiumCoreTests/STMFakePersisting+Async.m
@@ -1,10 +1,12 @@
-///
-///  STMFakePersisting+Async.m
-///  iSisSales
-///
-///  Generated with Handlebars on Thu, 09 Feb 2017 16:15:31 GMT
-///  Copyright © 2017 Sistemium UAB. All rights reserved.
-///
+//
+//  STMFakePersisting+Async.m
+//  iSisSales
+//
+//  Generated with HandlebarsGenerator on Thu, 09 Feb 2017 19:04:13 GMT
+//  Don't edit this file directly!
+//
+//  Copyright © 2017 Sistemium UAB. All rights reserved.
+//
 
 #import "STMFakePersisting+Async.h"
 
@@ -14,86 +16,86 @@
 @implementation STMFakePersisting (Async)
 
 - (void)findAsync:(NSString *)entityName identifier:(NSString *)identifier options:(NSDictionary *)options completionHandler:(STMPersistingAsyncDictionaryResultCallback)completionHandler {
-    
+
     dispatch_async(dispatch_get_global_queue(STM_FAKE_PERSISTING_ASYNC_DISPATCH_QUEUE, 0), ^{
-        
+
         NSError *error;
         NSDictionary * result = [self findSync:entityName identifier:identifier options:options error:&error];
         if (completionHandler) completionHandler(!error, result, error);
-        
-    });
+
+   });
     
 }
 
 - (void)findAllAsync:(NSString *)entityName predicate:(NSPredicate *)predicate options:(NSDictionary *)options completionHandler:(STMPersistingAsyncArrayResultCallback)completionHandler {
-    
+
     dispatch_async(dispatch_get_global_queue(STM_FAKE_PERSISTING_ASYNC_DISPATCH_QUEUE, 0), ^{
-        
+
         NSError *error;
         NSArray * result = [self findAllSync:entityName predicate:predicate options:options error:&error];
         if (completionHandler) completionHandler(!error, result, error);
-        
-    });
+
+   });
     
 }
 
 - (void)mergeAsync:(NSString *)entityName attributes:(NSDictionary *)attributes options:(NSDictionary *)options completionHandler:(STMPersistingAsyncDictionaryResultCallback)completionHandler {
-    
+
     dispatch_async(dispatch_get_global_queue(STM_FAKE_PERSISTING_ASYNC_DISPATCH_QUEUE, 0), ^{
-        
+
         NSError *error;
         NSDictionary * result = [self mergeSync:entityName attributes:attributes options:options error:&error];
         if (completionHandler) completionHandler(!error, result, error);
-        
-    });
+
+   });
     
 }
 
 - (void)mergeManyAsync:(NSString *)entityName attributeArray:(NSArray *)attributeArray options:(NSDictionary *)options completionHandler:(STMPersistingAsyncArrayResultCallback)completionHandler {
-    
+
     dispatch_async(dispatch_get_global_queue(STM_FAKE_PERSISTING_ASYNC_DISPATCH_QUEUE, 0), ^{
-        
+
         NSError *error;
         NSArray * result = [self mergeManySync:entityName attributeArray:attributeArray options:options error:&error];
         if (completionHandler) completionHandler(!error, result, error);
-        
-    });
+
+   });
     
 }
 
 - (void)destroyAsync:(NSString *)entityName identifier:(NSString *)identifier options:(NSDictionary *)options completionHandler:(STMPersistingAsyncNoResultCallback)completionHandler {
-    
+
     dispatch_async(dispatch_get_global_queue(STM_FAKE_PERSISTING_ASYNC_DISPATCH_QUEUE, 0), ^{
-        
+
         NSError *error;
         [self destroySync:entityName identifier:identifier options:options error:&error];
         if (completionHandler) completionHandler(!error, error);
-        
-    });
+
+   });
     
 }
 
 - (void)destroyAllAsync:(NSString *)entityName predicate:(NSPredicate *)predicate options:(NSDictionary *)options completionHandler:(STMPersistingAsyncIntegerResultCallback)completionHandler {
-    
+
     dispatch_async(dispatch_get_global_queue(STM_FAKE_PERSISTING_ASYNC_DISPATCH_QUEUE, 0), ^{
-        
+
         NSError *error;
         NSUInteger result = [self destroyAllSync:entityName predicate:predicate options:options error:&error];
         if (completionHandler) completionHandler(!error, result, error);
-        
-    });
+
+   });
     
 }
 
 - (void)updateAsync:(NSString *)entityName attributes:(NSDictionary *)attributes options:(NSDictionary *)options completionHandler:(STMPersistingAsyncDictionaryResultCallback)completionHandler {
-    
+
     dispatch_async(dispatch_get_global_queue(STM_FAKE_PERSISTING_ASYNC_DISPATCH_QUEUE, 0), ^{
-        
+
         NSError *error;
         NSDictionary * result = [self updateSync:entityName attributes:attributes options:options error:&error];
         if (completionHandler) completionHandler(!error, result, error);
-        
-    });
+
+   });
     
 }
 
--- iSistemiumCoreTests/STMFakePersisting+Promised.m
+++ iSistemiumCoreTests/STMFakePersisting+Promised.m
@@ -2,7 +2,9 @@
 //  STMFakePersisting+Promised.m
 //  iSisSales
 //
-//  Created by Alexander Levin on 06/02/2017.
+//  Generated with HandlebarsGenerator on Thu, 09 Feb 2017 19:04:13 GMT
+//  Don't edit this file directly!
+//
 //  Copyright © 2017 Sistemium UAB. All rights reserved.
 //
 
@@ -11,69 +13,139 @@
 #define STM_FAKE_PERSISTING_PROMISED_DISPATCH_QUEUE DISPATCH_QUEUE_PRIORITY_DEFAULT
 
 
-#define STMFakePersistingPromisedWithSyncScalar(returnType,methodName,signatureAttributes) \
-return [AnyPromise promiseWithResolverBlock:^(PMKResolver resolve){ \
-    dispatch_async(dispatch_get_global_queue(STM_FAKE_PERSISTING_PROMISED_DISPATCH_QUEUE, 0), ^{ \
-        NSError *error; \
-        returnType result = [self methodName##Sync:entityName signatureAttributes:signatureAttributes options:options error:&error]; \
-        if (error) resolve(error); else resolve(@(result)); \
-    }); \
-}];
-
-#define STMFakePersistingPromisedWithSync(returnType,methodName,signatureAttributes) \
-return [AnyPromise promiseWithResolverBlock:^(PMKResolver resolve){ \
-    dispatch_async(dispatch_get_global_queue(STM_FAKE_PERSISTING_PROMISED_DISPATCH_QUEUE, 0), ^{ \
-        NSError *error; \
-        returnType *result = [self methodName##Sync:entityName signatureAttributes:signatureAttributes options:options error:&error]; \
-        if (error) resolve(error); else resolve(result); \
-    }); \
-}];
-
 @implementation STMFakePersisting (Promised)
 
-#pragma mark - PersistingPromised implementation
+- (AnyPromise *)find:(NSString *)entityName identifier:(NSString *)identifier options:(STMPersistingOptions)options {
 
-- (AnyPromise *)find:(NSString *)entityName
-          identifier:(NSString *)identifier
-             options:(STMPersistingOptions)options {
-    
-    STMFakePersistingPromisedWithSync(NSDictionary,find,identifier)
-    
+    return [AnyPromise promiseWithResolverBlock:^(PMKResolver resolve){
+        dispatch_async(dispatch_get_global_queue(STM_FAKE_PERSISTING_PROMISED_DISPATCH_QUEUE, 0), ^{
+
+            NSError *error;
+            NSDictionary * result = [self findSync:entityName identifier:identifier options:options error:&error];
+
+            if (error) {
+                resolve(error);
+            } else {
+                resolve(result);
+            }
+
+        });
+    }];
+
 }
 
-- (AnyPromise *)findAll:(NSString *)entityName
-              predicate:(NSPredicate *)predicate
-                options:(STMPersistingOptions)options {
-    
-    STMFakePersistingPromisedWithSync(NSArray,findAll,predicate)
-    
+- (AnyPromise *)findAll:(NSString *)entityName predicate:(NSPredicate *)predicate options:(STMPersistingOptions)options {
+
+    return [AnyPromise promiseWithResolverBlock:^(PMKResolver resolve){
+        dispatch_async(dispatch_get_global_queue(STM_FAKE_PERSISTING_PROMISED_DISPATCH_QUEUE, 0), ^{
+
+            NSError *error;
+            NSArray * result = [self findAllSync:entityName predicate:predicate options:options error:&error];
+
+            if (error) {
+                resolve(error);
+            } else {
+                resolve(result);
+            }
+
+        });
+    }];
+
 }
 
-- (AnyPromise *)merge:(NSString *)entityName
-           attributes:(NSDictionary *)attributes
-              options:(STMPersistingOptions)options {
-    STMFakePersistingPromisedWithSync(NSDictionary,merge,attributes)
+- (AnyPromise *)merge:(NSString *)entityName attributes:(NSDictionary *)attributes options:(STMPersistingOptions)options {
+
+    return [AnyPromise promiseWithResolverBlock:^(PMKResolver resolve){
+        dispatch_async(dispatch_get_global_queue(STM_FAKE_PERSISTING_PROMISED_DISPATCH_QUEUE, 0), ^{
+
+            NSError *error;
+            NSDictionary * result = [self mergeSync:entityName attributes:attributes options:options error:&error];
+
+            if (error) {
+                resolve(error);
+            } else {
+                resolve(result);
+            }
+
+        });
+    }];
+
 }
 
-- (AnyPromise *)mergeMany:(NSString *)entityName
-           attributeArray:(NSArray *)attributeArray
-                  options:(STMPersistingOptions)options {
-    
-    STMFakePersistingPromisedWithSync(NSArray,mergeMany,attributeArray)
+- (AnyPromise *)mergeMany:(NSString *)entityName attributeArray:(NSArray *)attributeArray options:(STMPersistingOptions)options {
+
+    return [AnyPromise promiseWithResolverBlock:^(PMKResolver resolve){
+        dispatch_async(dispatch_get_global_queue(STM_FAKE_PERSISTING_PROMISED_DISPATCH_QUEUE, 0), ^{
+
+            NSError *error;
+            NSArray * result = [self mergeManySync:entityName attributeArray:attributeArray options:options error:&error];
+
+            if (error) {
+                resolve(error);
+            } else {
+                resolve(result);
+            }
+
+        });
+    }];
+
 }
 
-- (AnyPromise *)destroy:(NSString *)entityName
-             identifier:(NSString *)identifier
-                options:(STMPersistingOptions)options {
-    
-    STMFakePersistingPromisedWithSyncScalar(BOOL,destroy,identifier)
+- (AnyPromise *)destroy:(NSString *)entityName identifier:(NSString *)identifier options:(STMPersistingOptions)options {
+
+    return [AnyPromise promiseWithResolverBlock:^(PMKResolver resolve){
+        dispatch_async(dispatch_get_global_queue(STM_FAKE_PERSISTING_PROMISED_DISPATCH_QUEUE, 0), ^{
+
+            NSError *error;
+            BOOL result = [self destroySync:entityName identifier:identifier options:options error:&error];
+
+            if (error) {
+                resolve(error);
+            } else {
+                resolve(@(result));
+            }
+
+        });
+    }];
+
 }
 
-- (AnyPromise *)destroyAll:(NSString *)entityName
-                 predicate:(NSPredicate *)predicate
-                   options:(STMPersistingOptions)options {
-    STMFakePersistingPromisedWithSyncScalar(NSUInteger,destroyAll,predicate)
+- (AnyPromise *)destroyAll:(NSString *)entityName predicate:(NSPredicate *)predicate options:(STMPersistingOptions)options {
+
+    return [AnyPromise promiseWithResolverBlock:^(PMKResolver resolve){
+        dispatch_async(dispatch_get_global_queue(STM_FAKE_PERSISTING_PROMISED_DISPATCH_QUEUE, 0), ^{
+
+            NSError *error;
+            NSUInteger result = [self destroyAllSync:entityName predicate:predicate options:options error:&error];
+
+            if (error) {
+                resolve(error);
+            } else {
+                resolve(@(result));
+            }
+
+        });
+    }];
+
+}
+
+- (AnyPromise *)update:(NSString *)entityName attributes:(NSDictionary *)attributes options:(STMPersistingOptions)options {
+
+    return [AnyPromise promiseWithResolverBlock:^(PMKResolver resolve){
+        dispatch_async(dispatch_get_global_queue(STM_FAKE_PERSISTING_PROMISED_DISPATCH_QUEUE, 0), ^{
+
+            NSError *error;
+            NSDictionary * result = [self updateSync:entityName attributes:attributes options:options error:&error];
+
+            if (error) {
+                resolve(error);
+            } else {
+                resolve(result);
+            }
+
+        });
+    }];
+
 }
 
 @end
-
