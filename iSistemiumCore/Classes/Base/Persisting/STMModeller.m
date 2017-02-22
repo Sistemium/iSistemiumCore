094535ebe61a7b29b27caab1003a42b8782f9fbc
gogbajbobo <grigoriev.maxim@gmail.com>
2017-02-22 18:14:57 +0300

    parseMappingModel:

--- iSistemiumCore/Classes/Base/Persisting/STMModeller.m
+++ iSistemiumCore/Classes/Base/Persisting/STMModeller.m
@@ -35,7 +35,11 @@
     
     if (path) {
         
-        [self checkDataModelsWithBundlePath:path];
+        NSMappingModel *mappingModel = [self checkDataModelsWithBundlePath:path];
+        
+        if (mappingModel) {
+            [self parseMappingModel:mappingModel];
+        }
         
         NSManagedObjectModel *model = [self modelWithPath:path];
         
@@ -48,11 +52,130 @@
     
 }
 
-+ (void)checkDataModelsWithBundlePath:(NSString *)bundlePath {
++ (void)parseMappingModel:(NSMappingModel *)mappingModel {
+    
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"mappingType != %d", NSCopyEntityMappingType];
+    NSArray *changedEntityMappings = [mappingModel.entityMappings filteredArrayUsingPredicate:predicate];
+    
+    NSArray *entityMappingTypes = @[@(NSAddEntityMappingType),
+                                    @(NSCustomEntityMappingType),
+                                    @(NSRemoveEntityMappingType),
+                                    @(NSTransformEntityMappingType),
+                                    @(NSUndefinedEntityMappingType)];
+    
+    for (NSNumber *mapType in entityMappingTypes) {
+        
+        NSUInteger mappingType = mapType.integerValue;
+
+        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"mappingType == %d", mappingType];
+        NSArray *result = [changedEntityMappings filteredArrayUsingPredicate:predicate];
+        
+        if (result.count) {
+        
+            switch (mappingType) {
+                case NSAddEntityMappingType:
+                    [self parseAddEntityMappings:result];
+                    break;
+
+                case NSCustomEntityMappingType:
+                    [self parseCustomEntityMappings:result];
+                    break;
+
+                case NSRemoveEntityMappingType:
+                    [self parseRemoveEntityMappings:result];
+                    break;
+
+                case NSTransformEntityMappingType:
+                    [self parseTransformEntityMappings:result];
+                    break;
+
+                case NSUndefinedEntityMappingType:
+                    [self parseUndefinedEntityMappings:result];
+                    break;
+
+                default:
+                    break;
+            }
+
+        }
+        
+    }
+    
+}
+
++ (void)parseAddEntityMappings:(NSArray *)addEntityMappings {
+    
+//    NSLog(@"addEntityMappings %@", addEntityMappings);
+    NSLog(@"!!! next entities should be added: ");
+    
+    for (NSEntityMapping *entityMapping in addEntityMappings) {
+        
+        NSLog(@"!!! add %@", entityMapping.destinationEntityName);
+        
+    }
+    
+}
+
++ (void)parseCustomEntityMappings:(NSArray *)customEntityMappings {
+    NSLog(@"customEntityMappings %@", customEntityMappings);
+}
+
++ (void)parseRemoveEntityMappings:(NSArray *)removeEntityMappings {
+
+//    NSLog(@"removeEntityMappings %@", removeEntityMappings);
+    NSLog(@"!!! next entities should be removed: ");
+    
+    for (NSEntityMapping *entityMapping in removeEntityMappings) {
+        
+        NSLog(@"!!! remove %@", entityMapping.sourceEntityName);
+        
+    }
+
+}
+
++ (void)parseTransformEntityMappings:(NSArray *)transformEntityMappings {
+
+//    NSLog(@"transformEntityMappings %@", transformEntityMappings);
+    NSLog(@"!!! next entities should be transformed: ");
+    
+    for (NSEntityMapping *entityMapping in transformEntityMappings) {
+        
+        NSLog(@"!!! transform %@", entityMapping.destinationEntityName);
+
+        NSSet *addedProperties = entityMapping.userInfo[@"addedProperties"];
+        if (addedProperties.count) {
+            for (NSString *propertyName in addedProperties) {
+                NSLog(@"    !!! add property: %@", propertyName);
+            }
+        }
+
+        NSSet *removedProperties = entityMapping.userInfo[@"removedProperties"];
+        if (removedProperties.count) {
+            for (NSString *propertyName in removedProperties) {
+                NSLog(@"    !!! remove property: %@", propertyName);
+            }
+        }
+
+        NSSet *mappedProperties = entityMapping.userInfo[@"mappedProperties"];
+        if (mappedProperties.count) {
+            for (NSString *propertyName in mappedProperties) {
+                NSLog(@"    !!! remains the same property: %@", propertyName);
+            }
+        }
+
+    }
+
+}
+
++ (void)parseUndefinedEntityMappings:(NSArray *)undefinedEntityMappings {
+    NSLog(@"undefinedEntityMappings %@", undefinedEntityMappings);
+}
+
++ (NSMappingModel *)checkDataModelsWithBundlePath:(NSString *)bundlePath {
     
     NSString *modelDirInDocuments = [[STMFunctions documentsDirectory] stringByAppendingPathComponent:@"model"];
 
-    if (![STMFunctions dirExistsOrCreateAtPath:modelDirInDocuments]) return;
+    if (![STMFunctions dirExistsOrCreateAtPath:modelDirInDocuments]) return nil;
 
     NSFileManager *fm = [NSFileManager defaultManager];
     
@@ -64,7 +187,7 @@
         
         [self copyModelToPath:modelDirInDocuments
                      fromPath:bundlePath];
-        return;
+        return nil;
         
     }
     
@@ -73,13 +196,28 @@
     if ([bundleModel isEqual:documentsModel]) {
         
         NSLog(@"model have no changes");
-        
-    } else {
-        
-        NSLog(@"!!! model have changes, old should be replaced with new one !!!");
+        return nil;
         
     }
 
+    NSLog(@"!!! model have changes, old should be replaced with new one !!!");
+    
+#warning - maybe copy new model to Documents only after successful creating of db with the new model
+//    [self copyModelToPath:modelDirInDocuments
+//                 fromPath:bundlePath];
+    
+    NSError *error = nil;
+    
+    NSMappingModel *mappingModel = [NSMappingModel inferredMappingModelForSourceModel:documentsModel
+                                                                     destinationModel:bundleModel
+                                                                                error:&error];
+    
+    if (!mappingModel) {
+        NSLog(@"mappingModel error: %@, userInfo: %@", error.localizedDescription, error.userInfo);
+    }
+
+    return mappingModel;
+    
 }
 
 + (NSManagedObjectModel *)modelWithPath:(NSString *)modelPath {
