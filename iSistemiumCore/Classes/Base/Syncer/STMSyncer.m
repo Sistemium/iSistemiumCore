e94be231363a3f39d0e7311d291ef9867d3df4f9
Alexander Levin <alevin.ru@gmail.com>
2017-02-22 13:35:15 +0200

    stopDefantomization if defantomizer asks not isDefantomizing Syncer

--- iSistemiumCore/Classes/Base/Syncer/STMSyncer.m
+++ iSistemiumCore/Classes/Base/Syncer/STMSyncer.m
@@ -572,11 +572,7 @@
 
 - (void)defantomizeEntityName:(NSString *)entityName identifier:(NSString *)identifier {
 
-    if (!self.isDefantomizing) {
-        return;
-    }
-    
-    entityName = [STMFunctions addPrefixToEntityName:entityName];
+    if (!self.isDefantomizing) return [self.defantomizingDelegate stopDefantomization];
     
     [self.socketTransport findAsync:entityName identifier:identifier options:nil completionHandlerWithHeaders:^(BOOL success, NSDictionary *result, NSDictionary *headers, NSError *error) {
 
