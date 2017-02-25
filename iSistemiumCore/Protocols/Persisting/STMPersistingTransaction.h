ca739f7fb296baa820ca06c8a6a9484cde6277b5
Alexander Levin <alevin.ru@gmail.com>
2017-02-25 16:06:12 +0200

    STMPersistingTransaction cleanup

--- iSistemiumCore/Protocols/Persisting/STMPersistingTransaction.h
+++ iSistemiumCore/Protocols/Persisting/STMPersistingTransaction.h
@@ -8,30 +8,44 @@
 
 #import <Foundation/Foundation.h>
 
+#define STMPerisistingResultSet NSArray <NSDictionary *> *
+
 @protocol STMPersistingTransaction
 
 @property (readonly) id <STMModelling> modellingDelegate;
 
-- (NSUInteger)destroyWithoutSave:(NSString *)entityName predicate:(NSPredicate *)predicate options:(NSDictionary *)options error:(NSError **)error;
+- (NSUInteger)destroyWithoutSave:(NSString *)entityName
+                       predicate:(NSPredicate *)predicate
+                         options:(NSDictionary *)options
+                           error:(NSError **)error;
 
-- (NSDictionary *)mergeWithoutSave:(NSString *)entityName attributes:(NSDictionary *)attributes options:(NSDictionary *)options error:(NSError **)error;
+- (NSDictionary *)mergeWithoutSave:(NSString *)entityName
+                        attributes:(NSDictionary *)attributes
+                           options:(NSDictionary *)options
+                             error:(NSError **)error;
 
 - (NSDictionary *)updateWithoutSave:(NSString *)entityName
                          attributes:(NSDictionary *)attributes
                             options:(NSDictionary *)options
                               error:(NSError **)error;
 
-- (NSUInteger)count:(NSString *)entityName predicate:(NSPredicate *)predicate options:(NSDictionary *)options error:(NSError **)error;
+- (NSUInteger)count:(NSString *)entityName
+          predicate:(NSPredicate *)predicate
+            options:(NSDictionary *)options
+              error:(NSError **)error;
 
 @optional
 
-- (NSArray <NSDictionary *> *)findAllSync:(NSString *)entityName predicate:(NSPredicate *)predicate options:(NSDictionary *)options error:(NSError **)error;
+- (STMPerisistingResultSet)findAllSync:(NSString *)entityName
+                             predicate:(NSPredicate *)predicate
+                               options:(NSDictionary *)options
+                                 error:(NSError **)error;
 
-- (NSArray *)findAllSync:(NSString *)name
-               predicate:(NSPredicate *)predicate
-                 orderBy:(NSString *)orderBy
-               ascending:(BOOL)ascending
-              fetchLimit:(NSUInteger)fetchLimit
-             fetchOffset:(NSUInteger)fetchOffset;
+- (STMPerisistingResultSet)findAllSync:(NSString *)name
+                             predicate:(NSPredicate *)predicate
+                               orderBy:(NSString *)orderBy
+                             ascending:(BOOL)ascending
+                            fetchLimit:(NSUInteger)fetchLimit
+                           fetchOffset:(NSUInteger)fetchOffset;
 
 @end
