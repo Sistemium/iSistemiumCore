7c3dafb6f0691a68c8ccbbf0f8771fde725d1880
gogbajbobo <grigoriev.maxim@gmail.com>
2017-02-27 18:41:59 +0300

    add STMFiling protocol

---
+++ iSistemiumCore/Protocols/STMFiling.h
@@ -0,0 +1,40 @@
+//
+//  STMFiling.h
+//  iSisSales
+//
+//  Created by Maxim Grigoriev on 27/02/2017.
+//  Copyright © 2017 Sistemium UAB. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@protocol STMDirectoring <NSObject>
+
+@optional
+
+- (NSString *)userDocuments;
+- (NSString *)sharedDocuments;
+- (NSBundle *)bundle;
+
+
+@end
+
+
+@protocol STMFiling <NSObject>
+
+@optional
+
+@property (nonatomic, weak) id <STMDirectoring> directoring;
+@property (nonatomic, weak) NSFileManager *fileManager;
+
+- (NSString *)persistencePath:(NSString *)folderName;
+- (NSString *)picturesPath:(NSString *)folderName;
+- (NSString *)webViewsPath:(NSString *)folderName;
+- (NSString *)persistencePath:(NSString *)folderName;
+
+- (BOOL)copyFile:(NSString *)filePath toPath:(NSString *)newPath;
+
+- (NSString *)bundledModelFile:(NSString *)name;
+
+
+@end
