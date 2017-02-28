f5dadf2aa3516d038f80a9640d9b5183d44e2aad
gogbajbobo <grigoriev.maxim@gmail.com>
2017-02-28 15:01:10 +0300

    implement STMDirectoring

--- iSistemiumCoreTests/FilingTests.m
+++ iSistemiumCoreTests/FilingTests.m
@@ -23,10 +23,66 @@
 
 @interface STMDirectoring : NSObject <STMDirectoring>
 
+@property (nonatomic, strong) NSString *org;
+@property (nonatomic, strong) NSString *uid;
+@property (nonatomic, strong) NSString *documentsPath;
+@property (nonatomic, strong) NSString *orgPath;
+
+
 @end
 
 
 @implementation STMDirectoring
+
+#pragma mark - STMDirectoring
+
+- (instancetype)initWithOrg:(NSString *)org userId:(NSString *)uid {
+    
+    self = [super init];
+    
+    if (self) {
+        
+        self.org = org;
+        self.uid = uid;
+        
+    }
+    return self;
+    
+}
+
+- (NSString *)documentsPath {
+    
+    if (!_documentsPath) {
+        _documentsPath = [STMFunctions documentsDirectory];
+    }
+    return _documentsPath;
+    
+}
+
+- (void)setOrg:(NSString *)org {
+    
+    _org = org;
+    
+    self.orgPath = [self.documentsPath stringByAppendingPathComponent:org];
+    
+    [STMFunctions dirExistsOrCreateItAtPath:self.orgPath];
+    
+}
+
+- (NSString *)userDocuments {
+
+    NSString *userPath = [self.orgPath stringByAppendingPathComponent:self.uid];
+    return [STMFunctions dirExistsOrCreateItAtPath:userPath] ? userPath : nil;
+    
+}
+
+- (NSString *)sharedDocuments {
+
+    NSString *sharedPath = [self.orgPath stringByAppendingPathComponent:SHARED_PATH];
+    return [STMFunctions dirExistsOrCreateItAtPath:sharedPath] ? sharedPath : nil;
+
+}
+
 
 @end
 
