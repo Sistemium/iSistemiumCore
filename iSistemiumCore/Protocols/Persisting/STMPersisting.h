f78e9fb250f1c144493bca2854c504294ec42097
Alexander Levin <alevin.ru@gmail.com>
2017-03-05 20:55:59 +0200

    STMPersistingRelationshipSuffix constant

--- iSistemiumCore/Protocols/Persisting/STMPersisting.h
+++ iSistemiumCore/Protocols/Persisting/STMPersisting.h
@@ -22,6 +22,7 @@ typedef NS_ENUM(NSInteger, STMStorageType) {
 #define STMPersistingKeyCreationTimestamp @"deviceCts"
 #define STMPersistingKeyPhantom @"isFantom"
 
+#define STMPersistingRelationshipSuffix @"Id"
 
 #define STMPersistingOptions NSDictionary *
 
