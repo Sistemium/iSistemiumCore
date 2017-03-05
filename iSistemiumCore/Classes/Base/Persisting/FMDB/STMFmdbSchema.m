6dfa9f22cff8f330bc7c299cff878f3349357881
Alexander Levin <alevin.ru@gmail.com>
2017-03-05 20:57:19 +0200

    STMFmdbSchema createTables granulated to columns

--- iSistemiumCore/Classes/Base/Persisting/FMDB/STMFmdbSchema.m
+++ iSistemiumCore/Classes/Base/Persisting/FMDB/STMFmdbSchema.m
@@ -8,9 +8,15 @@
 
 #import "STMFmdbSchema.h"
 #import "STMFunctions.h"
-#import "STMPredicateToSQL.h"
+#import "FMDatabaseAdditions.h"
 
+#define SQLiteText @"TEXT"
+#define SQLiteInt @"INTEGER"
+#define SQLiteNumber @"NUMERIC"
 
+#define SQLiteDefaultNow @"DEFAULT(STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW'))"
+
+#define SQLiteStatementSeparator @"; "
 
 @interface STMFmdbSchema()
 
@@ -35,28 +41,27 @@
 }
 
 
-- (NSString *)sqliteTypeForAttribute:(NSAttributeDescription *)attribute {
+- (NSString *)sqliteTypeForAttributeType:(NSAttributeType)attributeType {
     
-    switch (attribute.attributeType) {
+    switch (attributeType) {
         case NSStringAttributeType:
         case NSDateAttributeType:
         case NSUndefinedAttributeType:
         case NSBinaryDataAttributeType:
         case NSTransformableAttributeType:
-            return @"TEXT";
+            return SQLiteText;
         case NSInteger64AttributeType:
         case NSBooleanAttributeType:
         case NSObjectIDAttributeType:
         case NSInteger16AttributeType:
         case NSInteger32AttributeType:
-            return @"INTEGER";
+            return SQLiteInt;
         case NSDecimalAttributeType:
         case NSFloatAttributeType:
         case NSDoubleAttributeType:
-            return @"NUMERIC";
-            break;
+            return SQLiteNumber;
         default:
-            return @"TEXT";
+            return SQLiteText;
     }
     
 }
@@ -64,122 +69,69 @@
 
 - (NSDictionary*)createTablesWithModelling:(id <STMModelling>)modelling {
     
-    NSDictionary <NSString *, NSEntityDescription *> *entities = modelling.entitiesByName;
+    NSArray *builtInAttributes = @[STMPersistingKeyPrimary,
+                                   STMPersistingKeyCreationTimestamp,
+                                   STMPersistingKeyVersion,
+                                   STMPersistingOptionLts,
+                                   STMPersistingKeyPhantom];
     
-    NSArray *ignoredAttributes = @[@"xid", @"id"];
+    NSArray *ignoredAttributes = [builtInAttributes arrayByAddingObjectsFromArray:@[@"xid"]];
     
     NSMutableDictionary *columnsDictionary = @{}.mutableCopy;
-    
-    NSString *createIndexFormat = @"CREATE INDEX IF NOT EXISTS %@_%@ on %@ (%@);";
-    NSString *fkColFormat = @"%@ TEXT REFERENCES %@(id) ON DELETE %@";
-    NSString *createTableFormat = @"CREATE TABLE IF NOT EXISTS %@ (id TEXT PRIMARY KEY";
-    
+
     NSString *createLtsTriggerFormat = @"CREATE TRIGGER IF NOT EXISTS %@_check_lts BEFORE UPDATE OF lts ON %@ FOR EACH ROW WHEN OLD.deviceTs > OLD.lts BEGIN SELECT RAISE(ABORT, 'ignored') WHERE OLD.deviceTs <> NEW.lts; END";
-    
-    NSString *createFantomTriggerFormat = @"CREATE TRIGGER IF NOT EXISTS %@_fantom_%@ BEFORE INSERT ON %@ FOR EACH ROW WHEN NEW.%@ is not null BEGIN INSERT INTO %@ (id, isFantom, lts, deviceTs) SELECT NEW.%@, 1, null, null WHERE NOT EXISTS (SELECT * FROM %@ WHERE id = NEW.%@); END";
-    
-    NSString *updateFantomTriggerFormat = @"CREATE TRIGGER IF NOT EXISTS %@_fantom_%@_update BEFORE UPDATE OF %@ ON %@ FOR EACH ROW WHEN NEW.%@ is not null BEGIN INSERT INTO %@ (id, isFantom, lts, deviceTs) SELECT NEW.%@, 1, null, null WHERE NOT EXISTS (SELECT * FROM %@ WHERE id = NEW.%@); END";
     
     NSString *fantomIndexFormat = @"CREATE INDEX IF NOT EXISTS %@_isFantom on %@ (isFantom);";
     
-    NSString *createCascadeTriggerFormat = @"DROP TRIGGER IF EXISTS %@_cascade_%@; CREATE TRIGGER IF NOT EXISTS %@_cascade_%@ BEFORE DELETE ON %@ FOR EACH ROW BEGIN DELETE FROM %@ WHERE %@ = OLD.id; END";
+    NSString *cascadeTriggerFormat = @"DROP TRIGGER IF EXISTS %@_cascade_%@; CREATE TRIGGER IF NOT EXISTS %@_cascade_%@ BEFORE DELETE ON %@ FOR EACH ROW BEGIN DELETE FROM %@ WHERE %@ = OLD.id; END";
     
     NSString *isRemovedTriggerFormat = @"CREATE TRIGGER IF NOT EXISTS %@_isRemoved BEFORE INSERT ON %@ FOR EACH ROW BEGIN SELECT RAISE(IGNORE) FROM RecordStatus WHERE isRemoved = 1 AND objectXid = NEW.id LIMIT 1; END";
     
-    for (NSString *entityName in entities){
+    for (NSString *entityName in modelling.entitiesByName){
         
         if ([modelling storageForEntityName:entityName] != STMStorageTypeFMDB){
             NSLog(@"STMFmdb ignore entity: %@", entityName);
             continue;
         }
         
+        
+        NSMutableArray <NSString *> *columns = builtInAttributes.mutableCopy;
         NSString *tableName = [STMFunctions removePrefixFromEntityName:entityName];
-        NSMutableArray *columns = @[].mutableCopy;
-        NSString *sql_stmt = [NSString stringWithFormat:createTableFormat, tableName];
+        BOOL tableExisted = [self.database tableExists:tableName];
         
-        NSDictionary *tableColumns = [modelling fieldsForEntityName:entityName];
-        
-        for (NSString* columnName in tableColumns.allKeys){
+        if (!tableExisted) {
             
-            if ([ignoredAttributes containsObject:columnName]) continue;
-            
-            [columns addObject:columnName];
-            
-            NSAttributeDescription* atribute = tableColumns[columnName];
-            
-            NSMutableArray <NSString*> *columnDefinition = @[@","].mutableCopy;
-            
-            [columnDefinition addObject:[STMPredicateToSQL quotedName:columnName]];
-            [columnDefinition addObject:[self sqliteTypeForAttribute:atribute]];
-            
-            if ([columnName isEqualToString:@"deviceCts"]) {
-                [columnDefinition addObject:@"DEFAULT(STRFTIME('%Y-%m-%d %H:%M:%f', 'NOW'))"];
-            }
-            
-            if ([columnName isEqualToString:STMPersistingOptionLts]) {
-                [columnDefinition addObject:@"DEFAULT('')"];
-            }
-            
-            NSString* unique = [atribute.userInfo valueForKey:@"UNIQUE"];
-            
-            if (unique) {
-                [columnDefinition addObject:[NSString stringWithFormat:@"UNIQUE ON CONFLICT %@", unique]];
-            }
-            
-            sql_stmt = [sql_stmt stringByAppendingString:[columnDefinition componentsJoinedByString:@" "]];
+            [self executeDDL:[self createTableDDL:tableName]];
+            [self executeDDL:[NSString stringWithFormat:fantomIndexFormat, tableName, tableName]];
+            [self executeDDL:[NSString stringWithFormat:createLtsTriggerFormat, tableName, tableName, tableName]];
+            [self executeDDL:[NSString stringWithFormat:isRemovedTriggerFormat, tableName, tableName]];
             
         }
         
-        NSDictionary *relationships = [modelling toOneRelationshipsForEntityName:entityName];
+        NSArray *columnAttributes = [modelling fieldsForEntityName:entityName].allValues;
+        NSPredicate *excludeBuiltIn = [NSPredicate predicateWithFormat:@"NOT (name IN %@)", ignoredAttributes];
         
-        for (NSString* entityKey in relationships.allKeys){
-            
-            sql_stmt = [sql_stmt stringByAppendingString:@", "];
-            
-            NSString *fkColumn = [entityKey stringByAppendingString:RELATIONSHIP_SUFFIX];
-            
-            NSString *fkTable = [STMFunctions removePrefixFromEntityName:relationships[entityKey]];
-            
-            NSString *cascadeAction = @"SET NULL";
-            NSString *fkSQL = [NSString stringWithFormat:fkColFormat, fkColumn, fkTable, cascadeAction];
-            
-            [columns addObject:fkColumn];
-            sql_stmt = [sql_stmt stringByAppendingString:fkSQL];
+        columnAttributes = [columnAttributes filteredArrayUsingPredicate:excludeBuiltIn];
+        
+        for (NSAttributeDescription *attribute in columnAttributes) {
+            [columns addObject:attribute.name];
+            // if the column exists we get an error
+            // tableExisted check will replaced by modelMapping
+            if (!tableExisted) {
+                [self executeDDL:[self addAttributeDDL:attribute tableName:tableName]];
+            }
+        }
+        
+        NSArray *relationships = [modelling objectRelationshipsForEntityName:entityName isToMany:@NO cascade:nil].allValues;
+        
+        for (NSRelationshipDescription *relationship in relationships) {
+            [columns addObject:[relationship.name stringByAppendingString:STMPersistingRelationshipSuffix]];
+            if (!tableExisted) {
+                [self executeDDL:[self addRelationshipDDL:relationship tableName:tableName]];
+            }
         }
         
         columnsDictionary[tableName] = columns.copy;
-        
-        sql_stmt = [sql_stmt stringByAppendingString:@" ); "];
-        
-        [self executeDDL:sql_stmt];
-        
-        sql_stmt = [NSString stringWithFormat:fantomIndexFormat, tableName, tableName];
-        [self executeDDL:sql_stmt];
-        
-        sql_stmt = [NSString stringWithFormat:createLtsTriggerFormat, tableName, tableName, tableName];
-        [self executeDDL:sql_stmt];
-        
-        sql_stmt = [NSString stringWithFormat:isRemovedTriggerFormat, tableName, tableName];
-        [self executeDDL:sql_stmt];
-        
-        for (NSString* entityKey in [modelling toOneRelationshipsForEntityName:entityName].allKeys){
-            NSString *fkColumn = [entityKey stringByAppendingString:RELATIONSHIP_SUFFIX];
-            
-            sql_stmt = [NSString stringWithFormat:createIndexFormat, tableName, entityKey, tableName, fkColumn];
-            
-            [self executeDDL:sql_stmt];
-            
-            NSString *fkTable = [STMFunctions removePrefixFromEntityName:[modelling toOneRelationshipsForEntityName:entityName][entityKey]];
-            
-            sql_stmt = [NSString stringWithFormat:createFantomTriggerFormat, tableName, fkColumn, tableName, fkColumn, fkTable, fkColumn, fkTable, fkColumn];
-            
-            [self executeDDL:sql_stmt];
-            
-            sql_stmt = [NSString stringWithFormat:updateFantomTriggerFormat, tableName, fkColumn, fkColumn, tableName, fkColumn, fkTable, fkColumn, fkTable, fkColumn];
-            
-            [self executeDDL:sql_stmt];
-            
-        }
         
         NSDictionary <NSString *, NSRelationshipDescription*> *cascadeRelations = [modelling objectRelationshipsForEntityName:entityName isToMany:@(YES) cascade:@YES];
         
@@ -187,23 +139,9 @@
             
             NSRelationshipDescription *relation = cascadeRelations[relationKey];
             NSString *childTableName = [STMFunctions removePrefixFromEntityName:relation.destinationEntity.name];
-            NSString *fkColumn = [relation.inverseRelationship.name stringByAppendingString:@"Id"];
+            NSString *fkColumn = [relation.inverseRelationship.name stringByAppendingString:STMPersistingRelationshipSuffix];
             
-            sql_stmt = [NSString stringWithFormat:createCascadeTriggerFormat, tableName, relationKey,tableName, relationKey,tableName, childTableName, fkColumn];
-            
-            [self executeDDL:sql_stmt];
-            
-        }
-        
-        for (NSString* columnName in tableColumns.allKeys){
-            
-            NSAttributeDescription* atribute = tableColumns[columnName];
-            
-            if (!atribute.indexed || [ignoredAttributes containsObject:columnName]) continue;
-            
-            sql_stmt = [NSString stringWithFormat:createIndexFormat, tableName, atribute.name, tableName, atribute.name];
-            
-            [self executeDDL:sql_stmt];
+            [self executeDDL:[NSString stringWithFormat:cascadeTriggerFormat, tableName, relationKey,tableName, relationKey,tableName, childTableName, fkColumn]];
             
         }
         
@@ -213,6 +151,105 @@
     
 }
 
+
+- (NSString *)createTableDDL:(NSString *)tableName {
+    
+    NSString *format = @"CREATE TABLE IF NOT EXISTS %@ (%@)";
+    
+    NSMutableArray *builtInColumns = [NSMutableArray array];
+    
+    [builtInColumns addObject:[self columnDDL:STMPersistingKeyPrimary datatype:SQLiteText constraints:@"PRIMARY KEY"]];
+    
+    [builtInColumns addObject:[self columnDDL:STMPersistingKeyCreationTimestamp datatype:SQLiteText constraints:SQLiteDefaultNow]];
+
+    [builtInColumns addObject:[self columnDDL:STMPersistingKeyVersion datatype:SQLiteText constraints:nil]];
+
+    [builtInColumns addObject:[self columnDDL:STMPersistingOptionLts datatype:SQLiteText constraints:@"DEFAULT('')"]];
+
+    [builtInColumns addObject:[self columnDDL:STMPersistingKeyPhantom datatype:SQLiteInt constraints:nil]];
+
+    return [NSString stringWithFormat:format, [self quoted:tableName], [builtInColumns componentsJoinedByString:@", "]];
+    
+}
+
+
+- (NSString *)createIndexDDL:(NSString *)tableName columnName:(NSString *)columnName {
+    NSString *format = @"CREATE INDEX IF NOT EXISTS %@_%@ on %@ (%@);";
+    return [NSString stringWithFormat:format, tableName, columnName, tableName, columnName];
+}
+
+- (NSString *)columnDDL:(NSString *)columnName datatype:(NSString *)datatype constraints:(NSString *)constraints {
+    
+    NSMutableArray *clauses = [NSMutableArray array];
+    
+    [clauses addObject:[self quoted:columnName]];
+    
+    if (datatype) [clauses addObject:datatype];
+    
+    if (constraints) [clauses addObject:constraints];
+    
+    return [clauses componentsJoinedByString:@" "];
+    
+}
+
+
+- (NSString *)addAttributeDDL:(NSAttributeDescription *)attribute tableName:(NSString *)tableName {
+    
+    NSString *dataType = [self sqliteTypeForAttributeType:attribute.attributeType];
+//    NSString *constraints = [attribute.userInfo valueForKey:@"UNIQUE"];
+//    
+//    if (constraints) {
+//        constraints = [NSString stringWithFormat:@"UNIQUE ON CONFLICT %@", constraints];
+//    }
+    
+    NSString *columnName = attribute.name;
+    
+    NSString *columnDDL = [self columnDDL:columnName datatype:dataType constraints:nil];
+    
+    NSString *format = @"ALTER TABLE %@ ADD COLUMN %@";
+    
+    NSMutableArray *clauses = [NSMutableArray arrayWithObject:[NSString stringWithFormat:format, tableName, columnDDL]];
+    
+    if (attribute.indexed) {
+        [clauses addObject:[self createIndexDDL:tableName columnName:columnName]];
+    }
+    
+    return [clauses componentsJoinedByString:SQLiteStatementSeparator];
+    
+}
+
+- (NSString *)addRelationshipDDL:(NSRelationshipDescription *)relationship tableName:(NSString *)tableName {
+    
+    NSString *columnName = [relationship.name stringByAppendingString:STMPersistingRelationshipSuffix];
+    NSString *parentName = [STMFunctions removePrefixFromEntityName:relationship.destinationEntity.name];
+    NSString *constraints = [NSString stringWithFormat:@"REFERENCES %@ ON DELETE SET NULL", parentName];
+    NSString *columnDDL = [self columnDDL:columnName datatype:SQLiteText constraints:constraints];
+    NSString *format = @"ALTER TABLE %@ ADD COLUMN %@";
+    
+    
+    NSMutableArray *clauses = [NSMutableArray arrayWithObject:[NSString stringWithFormat:format, tableName, columnDDL]];
+    
+    [clauses addObject:[self createIndexDDL:tableName columnName:columnName]];
+
+    // FIXME: too long line and too many parameters
+    NSString *phantomTriggerFormat = [NSString stringWithFormat:@"CREATE TRIGGER IF NOT EXISTS %%@_fantom_%%@ BEFORE %%@ ON %%@ FOR EACH ROW WHEN NEW.%%@ is not null BEGIN INSERT INTO %%@ (%@, %@, %@, %@) SELECT NEW.%%@, 1, null, null WHERE NOT EXISTS (SELECT * FROM %%@ WHERE %@ = NEW.%%@); END", STMPersistingKeyPrimary, STMPersistingKeyPhantom, STMPersistingOptionLts, STMPersistingKeyVersion, STMPersistingKeyPrimary];
+    
+    [clauses addObject:[NSString stringWithFormat:phantomTriggerFormat, tableName, parentName, @"INSERT", tableName, columnName, parentName, columnName, parentName, columnName]];
+    
+    
+    NSString *action = [@"UPDATE OF " stringByAppendingString:columnName];
+    
+    [clauses addObject:[NSString stringWithFormat:phantomTriggerFormat, tableName, [parentName stringByAppendingString:@"_update"], action, tableName, columnName, parentName, columnName, parentName, columnName]];
+    
+    
+    return [clauses componentsJoinedByString:SQLiteStatementSeparator];
+    
+}
+
+
+- (NSString *)quoted:(NSString *)aString {
+    return [NSString stringWithFormat:@"[%@]", aString];
+}
 
 - (BOOL)executeDDL:(NSString *)ddl {
     BOOL res = [self.database executeStatements:ddl];
