4346a1e100340ba0c92cacfe2b8fd933ca04091f
Edgar Jan <krodanix@gmail.com>
2017-03-06 16:41:26 +0200

    UITestSetup

--- iSistemiumCore/STMCoreAppDelegate.m
+++ iSistemiumCore/STMCoreAppDelegate.m
@@ -27,8 +27,7 @@
 
 #import "STMUserDefaults.h"
 
-#import "STMWorkflowController.h"
-#import "STMWorkflowEditablesVC.h"
+#import "UITestSetup.h"
 
 @interface STMCoreAppDelegate()
 
@@ -473,24 +472,7 @@
     
     if ([[[NSProcessInfo processInfo] arguments] containsObject:@"WorkflowTest"]){
         
-        UIViewController *vc = [UIApplication sharedApplication].keyWindow.rootViewController;
-        
-        NSString *processing = @"routing";
-        
-        NSString *workflow = @"{\"routing\": {\"label\": \"Раскладка\",\"desc\": \"Расставьте точки по порядку отгрузки\",\"cls\": \"blue\",\"messageCls\": \"blue\",\"editable\": true,\"from\": [\"rejectOrder\",\"finished\"]},\"confirmOrder\": {\"label\": \"Упорядочено\",\"desc\": \"Водитель определил порядок объезда\",\"cls\": \"blue\",\"messageCls\": \"blue\",\"from\": [\"routing\"],\"editables\": [\"commentText\"]},\"started\": {\"label\": \"Выполнение\",\"desc\": \"Водитель выполняет задания маршрута\",\"cls\": \"green\",\"messageCls\": \"blue\",\"from\": [\"confirmOrder\"],\"editable\": true},\"finished\": {\"label\": \"Завершено\",\"desc\": \"Водитель завершил маршрут\",\"from\": [\"started\"]},\"rejectOrder\": {\"label\": \"Отказаться\",\"desc\": \"Водитель отказался принять задания\",\"cls\": \"red\",\"messageCls\": \"blue\",\"from\": [\"routing\"],\"editables\": [\"commentText\"]}}";
-        
-        STMWorkflowAC *workflowActionSheet = [STMWorkflowController workflowActionSheetForProcessing:processing
-                                                                                          inWorkflow:workflow
-                                                                                         withHandler:^(UIAlertAction *action) {
-                                                                                             
-                                                                                             
-                                                                                         }];
-        
-        workflowActionSheet.popoverPresentationController.sourceView = vc.view;
-        workflowActionSheet.popoverPresentationController.sourceRect = vc.view.bounds;
-        workflowActionSheet.popoverPresentationController.permittedArrowDirections = 0;
-        
-        [vc presentViewController:workflowActionSheet animated:YES completion:nil];
+        [UITestSetup workflowSetup];
         
     }
     
