7db7bbe225ebceaa527ea9797f0e6ccb8ec37146
gogbajbobo <grigoriev.maxim@gmail.com>
2017-04-03 12:37:03 +0300

    do not change nonloadedPictures array if data have no thumbnailPath

--- iSistemiumCore/Classes/EntityControllers/STMCorePicturesController.m
+++ iSistemiumCore/Classes/EntityControllers/STMCorePicturesController.m
@@ -189,7 +189,11 @@
     
     self.nonloadedPicturesSubscriptionID = [self.persistenceDelegate observeEntityNames:self.pictureEntitiesNames.allObjects predicate:predicate callback:^(NSString *entityName, NSArray *data) {
         
-        NSString *dataId = data.firstObject[@"id"];
+        NSDictionary *pic = data.firstObject;
+        
+        if (!pic[@"thumbnailPath"]) return;
+        
+        NSString *dataId = pic[@"id"];
         
         NSPredicate *predicate = [NSPredicate predicateWithFormat:@"attributes.id != %@", dataId];
         self.nonloadedPictures = [self.nonloadedPictures filteredArrayUsingPredicate:predicate].mutableCopy;
