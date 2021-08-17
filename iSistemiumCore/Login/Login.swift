991025f8f88fdeab7aa0b1082b48130b91353f69
Edgar Jan <EdgarJan@users.noreply.github.com>
2021-08-17 16:25:39 +0300

    swiftui file

---
+++ iSistemiumCore/Login/Login.swift
@@ -0,0 +1,21 @@
+//
+//  Login.swift
+//  iSistemiumCore
+//
+//  Created by Edgar Jan Vuicik on 2021-08-17.
+//  Copyright © 2021 Sistemium UAB. All rights reserved.
+//
+
+import SwiftUI
+
+struct Login: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Login_Previews: PreviewProvider {
+    static var previews: some View {
+        Login()
+    }
+}
