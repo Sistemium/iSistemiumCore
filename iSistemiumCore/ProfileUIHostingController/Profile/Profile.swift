b22ea073eefc3fd9a063236c7ef2b35b212652f0
Edgar Jan <EdgarJan@users.noreply.github.com>
2021-10-25 16:41:45 +0300

    ProfileUIHostingController

---
+++ iSistemiumCore/ProfileUIHostingController/Profile/Profile.swift
@@ -0,0 +1,27 @@
+//
+//  Profile.swift
+//  iSisSales
+//
+//  Created by Edgar Jan Vuicik on 2021-10-25.
+//  Copyright © 2021 Sistemium UAB. All rights reserved.
+//
+
+import SwiftUI
+
+struct Profile: View {
+    var body: some View {
+        NavigationView{
+            
+        }
+        .navigationBarTitle("ENTER TO SISTEMIUM", displayMode: .inline)
+        .navigationBarItems(leading:
+            Button(action: {
+
+            
+            
+            }) {
+                Text("DEMO")
+            }
+        )
+    }
+}
